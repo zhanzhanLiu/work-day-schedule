a3d8edc8bb7ef05963703282db1911645412a162
zhanzhanLiu <zzliu@ucdavis.edu>
2021-11-20 04:44:24 -0600

    add background colors according to time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,24 @@
 // Date Display
 var timeEl = $("#currentDay")
-timeEl.text(moment().format("MMM Do YYYY"))
+timeEl.text(moment().format("dddd, MMMM Do, YYYY"))
 
 // Set up time schedule Display
 var timeTableEl = $("#time-table")
 function timeSlot() {
     for (var i = 9; i <= 17; i++){
+
+        // Create Row and Column Element
         var timeRow = $('<tr>');
         timeRow.appendTo(timeTableEl)
         var timeCol1 = $('<td>');
         var timeCol2 = $('<td>');
         var timeCol3 = $('<td>');
+
+        // Add width layout class to columns
         timeCol1.addClass("col-1")
         timeCol3.addClass("col-1")
+
+        // Time Column Display
         if (i < 12){
             timeCol1.text(i + "AM").appendTo(timeRow);
         }
@@ -22,7 +28,20 @@ function timeSlot() {
         else{
             timeCol1.text(i-12 + "PM").appendTo(timeRow);
         }
-        timeCol2.appendTo(timeRow);
+
+        // Background Color according to current time
+        var currentTime = moment().format("h"); //Obtain current time hour
+        console.log(currentTime)
+        if (currentTime > i) {
+            timeCol2.addClass("bg-secondary").appendTo(timeRow);
+        }
+        else if (currentTime === i) {
+            timeCol2.addClass("bg-danger").appendTo(timeRow);
+        }
+        else{
+            timeCol2.addClass("bg-success").appendTo(timeRow);
+        }
+
         timeCol3.appendTo(timeRow);
     }
     
