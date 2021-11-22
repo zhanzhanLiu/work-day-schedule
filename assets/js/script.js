7834a55e01d9c08e1ab010a8db5de9bb26453993
zhanzhanLiu <zzliu@ucdavis.edu>
2021-11-21 22:31:13 -0600

    save input to local storage and get when refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,10 @@ var timeEl = $("#currentDay")
 timeEl.text(moment().format("dddd, MMMM Do, YYYY"))
 
 // Set up time schedule Display
-var timeTableEl = $("#time-table")
+var timeTableEl = $("#time-table");
+var eventStorage = localStorage.getItem("eventSaving");
+eventStorage = JSON.parse(eventStorage) ?? {};
+var eventSaving = eventStorage;
 function timeSlot() {
     for (var i = 9; i <= 17; i++){
 
@@ -30,14 +33,13 @@ function timeSlot() {
         }
 
         // Add Background Color according to current time to Column 2
-        var currentTime = moment().format("h"); //Obtain current time hour
+        var currentTime = moment().format("H"); //Obtain current time hour
         var eventInput = $("<input>"); eventInput.addClass("form-control border-0");
-    
         if (currentTime > i) {
             timeCol2.addClass("bg-secondary").appendTo(timeRow);
             eventInput.addClass("bg-secondary").appendTo(timeCol2)
         }
-        else if (currentTime === i) {
+        else if (currentTime == i) {
             timeCol2.addClass("bg-danger").appendTo(timeRow);
             eventInput.addClass("bg-danger").appendTo(timeCol2)
         }
@@ -45,19 +47,60 @@ function timeSlot() {
             timeCol2.addClass("bg-success").appendTo(timeRow);
             eventInput.addClass("bg-success").appendTo(timeCol2)
         }
-        
+        $(eventInput).attr("id","input" + (i-9));
+        $(timeCol2).attr("id","row" + (i-9));
+
         
 
         // Add buttons to Column 3
-        var saveButton = $('<button>')
-        var saveIcon = $("<i>"); 
-        saveButton.addClass("btn btn-primary btn-control")
-        saveIcon.addClass("bi bi-save-fill").appendTo(saveButton)
-        $(saveIcon).attr("color", "blue");
-        saveButton.appendTo(timeCol3);
+        // var saveButton = $('<button>')
+        var saveIcon = $("<button>"); 
+        $(saveIcon).attr("id","button" + (i-9));
+        $(saveIcon).attr("value", "save");
+        // saveButton.addClass("btn btn-primary btn-control")
+        saveIcon.addClass("bi bi-save-fill bg-primary").appendTo(timeCol3)
+        // $(saveIcon).attr("color", "blue");
+        // saveButton.appendTo(timeCol3);
         timeCol3.addClass("border rounded-right bg-primary").appendTo(timeRow);
+
+        saveIcon.on("click", function(event) {
+            event.preventDefault()
+            
+            console.log(event.target);
+            buttonId = event.target.id;
+            console.log(buttonId.split(""))
+            var inputTimeSlot = buttonId.split("")[6];
+            console.log(inputTimeSlot);
+            var eventInputId = "input" + inputTimeSlot;
+            var eventInputEl = document.getElementById(eventInputId);
+            // var rowInputEl = document.getElementById("row"+inputTimeSlot);
+            // rowInputEl.textContent = eventInputEl.value;
+            console.log(eventInputEl.value);
+
+            var timeEvent= {
+                timeHour: inputTimeSlot,
+                event: eventInputEl.value
+            };
+            
+            eventSaving[eventInputId] = eventInputEl.value;
+            
+            localStorage.setItem("eventSaving", JSON.stringify(eventSaving));
+
+        })
+
+        // function userExists(i) {
+        //     return eventStorage.some(function(el) {
+        //     return el.id === i-9;
+        //     }); 
+        // }
+              
+        // console.log(userExists(i));
     }
-    
+
+    // Check if the event has been saved before
+    Object.keys(eventStorage).forEach(function(inputID) {
+        document.getElementById(inputID).value = eventStorage[inputID];
+    })
 
 }
 
